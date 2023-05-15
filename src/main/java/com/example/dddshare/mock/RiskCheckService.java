3d792356e69af3f2914b896b07a56276ea5b659b
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2023-05-15 23:25:32 +0800

    rename

--- src/main/java/com/example/dddshare/mock/RiskCheckService.java
+++ src/main/java/com/example/dddshare/mock/RiskCheckService.java
@@ -3,7 +3,7 @@ package com.example.dddshare.mock;
 import java.math.BigDecimal;
 
 public class RiskCheckService {
-    public String checkPayment(String userId, String merchantAccountId, BigDecimal amount) {
+    public String checkPayment(String userAccountId, String storeAccountId, BigDecimal amount) {
         return "0000";
     }
 }
