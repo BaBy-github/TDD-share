18ff0010703be7df024722917e8a9773bcc35ad8
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2023-05-15 23:18:03 +0800

    🥚实体
    将业务逻辑封装在一起，而不是散布在整个系统中

--- src/main/java/com/example/dddshare/三层架构/AccountEntity.java
+++ src/main/java/com/example/dddshare/三层架构/AccountEntity.java
@@ -1,25 +1,13 @@
 package com.example.dddshare.三层架构;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class AccountEntity {
-    private Integer id;
+    private String id;
     private BigDecimal amount;
-
-    /**
-     * 转出操作
-     */
-    public void withdraw(BigDecimal amount) {
-        this.amount = this.amount.subtract(amount);
-    }
-
-    /**
-     * 转入操作
-     */
-    public void deposit(BigDecimal amount) {
-        this.amount = this.amount.add(amount);
-    }
 }
